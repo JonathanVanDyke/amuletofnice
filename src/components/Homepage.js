db8e7248cbc4e5cc28ae6b5fc637bed00b8941b0
Jonathan <jfv213@gmail.com>
2023-01-16 14:20:10 -0500

    Refactor image options

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Screenshot from './Screenshot';
-import Dropdown from './Dropdown';
-import { BORDER_ART_LABELS } from '../constants/borderArts';
+import ImageOptions from './ImageOptions';
 
 const Homepage = () => {
   // DropDown
@@ -42,34 +41,15 @@ const Homepage = () => {
         topPos={topPos} 
         scale={scale}
       />
-      <div className='Image-Options'>
-      <br></br>
-      <div>
-        <div style={{ color: "white" }}>Scale</div>
-        <input type="range" min="0" max="3" step=".01" value={scale}
-          onChange={onSliderChange} />
-      </div>
-        <div className='Position-Options'>
-        <div>
-          <div style={{ color: "white" }}>Up-Down</div>
-          <input type="range" min="-200" max="200" step="1" value={topPos}
-            style={{ appearance: "slider-vertical" }}
-            onChange={onTopChange} />
-        </div>
-          <div style={{ marginTop: "50px" }}
->
-          <div style={{ color: "white" }}>Left-Right</div>
-          <input type="range" min="-200" max="200" step="1" value={rightPos}
-            onChange={onRightChange} />
-        </div>
-      </div>
-      <Dropdown 
-        placeholder="Select Border"
-        onDropDownSelect={onDropDownSelect} 
-        defaultValue='generic-attack-red'
-        options={BORDER_ART_LABELS}
+      <ImageOptions 
+        scale={scale}
+        topPos={topPos}
+        rightPos={rightPos}
+        onSliderChange={onSliderChange}
+        onTopChange={onTopChange}
+        onRightChange={onRightChange}
+        onDropDownSelect={onDropDownSelect}
       />
-      </div>
     </div>
   )
 }
---
+++ src/components/ImageOptions.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Dropdown from './Dropdown';
+import { BORDER_ART_LABELS } from '../constants/borderArts';
+
+const ImageOptions = ({ 
+  scale, 
+  topPos, 
+  rightPos, 
+  onSliderChange = () => {}, 
+  onTopChange = () => {}, 
+  onRightChange = () => {},
+  onDropDownSelect = () => {}
+}) => {
+
+
+  return (
+    <div className='Image-Options'>
+      <div>
+        <div style={{ color: "white" }}>Scale</div>
+        <input type="range" min="0" max="3" step=".01" value={scale}
+          onChange={onSliderChange} />
+      </div>
+      <div className='Position-Options'>
+        <div>
+          <div style={{ color: "white" }}>Up-Down</div>
+          <input type="range" min="-200" max="200" step="1" value={topPos}
+            style={{ appearance: "slider-vertical" }}
+            onChange={onTopChange} />
+        </div>
+        <div style={{ marginTop: "50px" }}>
+          <div style={{ color: "white" }}>Left-Right</div>
+          <input type="range" min="-200" max="200" step="1" value={rightPos}
+            onChange={onRightChange}
+          />
+        </div>
+      </div>
+      <Dropdown
+        placeholder="Select Border"
+        onDropDownSelect={onDropDownSelect}
+        defaultValue='generic-attack-red'
+        options={BORDER_ART_LABELS}
+      />
+    </div>
+  )
+}
+
+export default ImageOptions
