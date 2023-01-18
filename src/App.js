ac002e118430b3524ad883bde6ad757060d16637
Jonathan <jfv213@gmail.com>
2023-01-18 14:50:19 -0500

    Add analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import Homepage from "./components/Homepage";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "UA-151178591-2"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
